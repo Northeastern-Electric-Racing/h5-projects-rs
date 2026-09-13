135ca44b1a916f110851279c884e7cd6ac8718ba
Aolgeirson <asher@olgeirson.com>
2026-09-13 17:41:47 -0400

    inbox drafted; state machine framework

--- lightning/src/inbox.rs
+++ lightning/src/inbox.rs
@@ -1,10 +1,41 @@
+pub use self::inbox::FaultframeState;
 mod inbox {
     use can_handler::NerCan;
-    use embassy_sync::channel::Receiver;
+    use defmt::warn;
+    use embassy_stm32::can::Frame;
+    use embassy_sync::{
+        blocking_mutex::{
+            CriticalSectionMutex,
+            raw::{CriticalSectionRawMutex, ThreadModeRawMutex},
+        },
+        channel::Receiver,
+        mutex::Mutex,
+    };
+    const CAN_RECV_TIMEOUT: Duration = Duration::from_millis(500);
+    // Previously known as IMD_GENERAL_MSG_ID
+    const IMD_CAN_ID: Id = Id::Standard(0x37);
+    // BMS_LIGHTNING_OKAY_MSG_ID
+    const BMS_CAN_ID: Id = Id::Standard(0x37);
+
+    // #define RESET_LATCHING_MSG_ID     0x510
+    const LATCHING_CAN_ID: ID = 0x510;
+    use embassy_time::{Duration, WithTimeout};
+    use embedded_can::Id;
+    use heapless::mpmc::Queue;
     #[derive(Debug)]
-    pub struct FaultStates {
-        BMSFault: bool,
-        IMDFault: bool,
+    pub enum FaultframeState {
+        BMSFault,
+        BMSOk,
+        IMDFault,
+        IMDOk,
+        LatchingOk,
+        LatchingFault,
+    }
+
+    #[derive(Debug)]
+    pub enum CANError {
+        Timeout,
+        Unkown,
     }
 
     /// This struct requires an already existing NerCan instance, as well as an already spawned
@@ -14,8 +45,40 @@ mod inbox {
         receiver: Receiver<'static, ThreadModeRawMutex, Frame, 16>,
     }
     impl Inbox {
-        pub fn get_latest_unread(self) -> FaultStates {
-            // TODO: Handle this
+        // TODO: Make this a task
+        pub async fn populate_queue(
+            self,
+            quetex: &'static Mutex<CriticalSectionRawMutex, Queue<Option<FaultframeState>, 32>>,
+        ) -> ! {
+            loop {
+                let latest: Option<FaultframeState> =
+                    match self.receiver.receive().with_timeout(CAN_RECV_TIMEOUT).await {
+                        Ok(frame) => match frame.id() {
+                            &IMD_CAN_ID => match frame.data().iter().sum() {
+                                0 => Some(FaultframeState::IMDOk),
+                                _ => Some(FaultframeState::IMDFault),
+                            },
+                            &BMS_CAN_ID => match frame.data()[0] & 0x80 {
+                                0 => Some(FaultframeState::BMSOk),
+                                _ => Some(FaultframeState::BMSFault),
+                            },
+                            &LATCHING_CAN_ID => match frame.data()[0] & 0x80 {
+                                0 => Some(FaultframeState::LatchingOk),
+                                _ => Some(FaultframeState::LatchingFault),
+                            },
+                            _id => {
+                                warn!("Unknown ID: {:#?}", _id);
+                                None
+                            }
+                        },
+                        Err(e) => {
+                            warn!("Did not recive CAN Frame. Error: {}", e);
+                            None
+                        }
+                    };
+                quetex.lock().await.enqueue(latest).expect("Queue Full");
+                // TODO: This shouldn't be a panic
+            }
         }
     }
 }
--- lightning/src/state.rs
+++ lightning/src/state.rs
@@ -1,20 +1,39 @@
 // TODO: Handle state machine logic here
-// TODO: Fix imports
 mod state_machine {
-    use crate::inbox;
+    use crate::inbox::{
+        self,
+        FaultframeState::{self, BMSFault, IMDFault, LatchingFault},
+    };
     use embassy_executor::task;
-    use embassy_sync::mutex;
-    // TODO: Accept the ground truth from a mutex that gets populated from a seperate CAN thread
+    use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
+    use heapless::mpmc::Queue;
     #[embassy_executor::task]
     pub async fn state_machine(
-        faults: mutex::Mutex<CriticalSectionMutex, Result<inbox::FaultStatus, /*whatever the can error type is*/>>,
+        quetex: &'static Mutex<CriticalSectionRawMutex, Queue<Option<FaultframeState>, 32>>,
     ) -> ! {
         // TODO: Init; have both lights off here
         loop {
-            match faults {
-                // TODO: Act on hardware here
-                Ok(v) => todo!(),
-                Err(_) => //TODO: Red lights
+            match quetex.lock().await.dequeue() {
+                Some(latest) => {
+                    match latest {
+                        Some(msg) => {
+                            match msg {
+                                BMSFault | IMDFault | LatchingFault => {
+                                    todo!("set red")
+                                }
+                                _ => {
+                                    todo!("set green")
+                                }
+                            };
+                        }
+                        None =>
+                            // This means that the frame was unrelated or not found
+                            {}
+                    }
+                }
+                None =>
+                    // This means that the queue is empty. Check the old code for what to do here
+                    {}
             }
         }
     }
