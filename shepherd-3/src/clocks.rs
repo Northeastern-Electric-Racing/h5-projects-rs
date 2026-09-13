d5275ad71ae018da4c6c291d4b0a3f315e976875
Blake Jackson <blakejackson312006@gmail.com>
2026-09-13 14:16:48 -0400

    workspace, shepherd-3: fix merge commit

--- shepherd-3/src/clocks.rs
+++ shepherd-3/src/clocks.rs
@@ -21,37 +21,37 @@ pub fn rcc_config() -> embassy_stm32::Config {
 
     // PLL1: 25 / 2 = 12.5 MHz ref, x28 = 350 MHz VCO, /2 = 175 MHz.
     config.rcc.pll1 = Some(Pll {
-        source: PllSource::HSE,
-        prediv: PllPreDiv::DIV2,
-        mul: PllMul::MUL28,
-        divp: Some(PllDiv::DIV2),
-        divq: Some(PllDiv::DIV2),
-        divr: Some(PllDiv::DIV2),
+        source: PllSource::Hse,
+        prediv: PllPreDiv::Div2,
+        mul: PllMul::Mul28,
+        divp: Some(PllDiv::Div2),
+        divq: Some(PllDiv::Div2),
+        divr: Some(PllDiv::Div2),
     });
 
     // PLL2: 25 / 5 = 5 MHz ref, x64 = 320 MHz VCO, /5 = 64 MHz on P and Q.
     config.rcc.pll2 = Some(Pll {
-        source: PllSource::HSE,
-        prediv: PllPreDiv::DIV5,
-        mul: PllMul::MUL64,
-        divp: Some(PllDiv::DIV5),
-        divq: Some(PllDiv::DIV5),
-        divr: Some(PllDiv::DIV2),
+        source: PllSource::Hse,
+        prediv: PllPreDiv::Div5,
+        mul: PllMul::Mul64,
+        divp: Some(PllDiv::Div5),
+        divq: Some(PllDiv::Div5),
+        divr: Some(PllDiv::Div2),
     });
 
-    config.rcc.sys = Sysclk::PLL1_P;
-    config.rcc.ahb_pre = AHBPrescaler::DIV1;
-    config.rcc.apb1_pre = APBPrescaler::DIV1;
-    config.rcc.apb2_pre = APBPrescaler::DIV1;
-    config.rcc.apb3_pre = APBPrescaler::DIV1;
+    config.rcc.sys = Sysclk::Pll1P;
+    config.rcc.ahb_pre = AHBPrescaler::Div1;
+    config.rcc.apb1_pre = APBPrescaler::Div1;
+    config.rcc.apb2_pre = APBPrescaler::Div1;
+    config.rcc.apb3_pre = APBPrescaler::Div1;
 
     // TSECU uses PWR_REGULATOR_VOLTAGE_SCALE1 (max 200 MHz).
     config.rcc.voltage_scale = VoltageScale::Scale1;
 
     // Kernel clock muxes. these should match PeriphCommonClock_Config() from TSECU-Shepherd.
-    config.rcc.mux.fdcan12sel = mux::Fdcansel::PLL2_Q;
-    config.rcc.mux.spi1sel = mux::Spi1sel::PLL2_P;
-    config.rcc.mux.spi2sel = mux::Spi2sel::PLL2_P;
+    config.rcc.mux.fdcan12sel = mux::Fdcansel::Pll2Q;
+    config.rcc.mux.spi1sel = mux::Spi1sel::Pll2P;
+    config.rcc.mux.spi2sel = mux::Spi2sel::Pll2P;
 
     config
 }
