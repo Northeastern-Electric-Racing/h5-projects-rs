8a279bef72bb62a01bfbd6fc046ce904cbd055fb
bjackson312006 <blakejackson312006@gmail.com>
2026-09-25 19:06:53 -0400

    shepherd-3: faults: add filter to can

--- shepherd-3/src/can.rs
+++ shepherd-3/src/can.rs
@@ -39,12 +39,16 @@ mod api {
     /// Initializes CAN and starts up the CAN handler.
     #[embassy_executor::task]
     pub async fn can_task(spawner: embassy_executor::Spawner, r: crate::CanResources) {
-        use embassy_stm32::can::CanConfigurator;
+        use embassy_stm32::can::{CanConfigurator};
+        use embassy_stm32::can::filter::{StandardFilterSlot};
+
+        /// CAN ID for DTI status message. We use this to make sure we are always triggering our CAN interrupt (which this will do because
+        /// this message always gets recieved). We need to do this because it protects against our TX waker from going to sleep forever,
+        /// which happens right now for some reason. Ideally this will be a temporary fix.
+        const DTI_ERPM_STATUS_MESSAGE: u16 = 0x416;
 
         let configurator = CanConfigurator::new(r.can, r.can_rx, r.can_tx, interrupts::Irqs);
-        let can = handler::NerCan::init(configurator);
-
-        // u_TODO probably should add can fitlers and such here
+        let can = handler::NerCan::init(configurator).add_standard_filter(StandardFilterSlot::_0, DTI_ERPM_STATUS_MESSAGE, None);
 
         let (tx, rx, props) = can.start();
 
